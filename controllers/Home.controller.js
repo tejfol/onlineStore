64e11aa223ca0487462402b379f24714b84e71ae
tejfol <tejfolcss@gmail.com>
2021-04-28 15:49:39 +0300

    Filter added

--- controllers/Home.controller.js
+++ controllers/Home.controller.js
@@ -7,4 +7,32 @@ module.exports = {
         });
         return res.status(200).render("pages/index", { allItems: allItems });
     },
+    filter: async (req, res) => {
+        const { filter } = req.query;
+        switch (filter) {
+            case "newest":
+                const filteredItems = await Items.findAll({
+                    order: [["updatedAt", "DESC"]],
+                });
+                return res
+                    .status(200)
+                    .render("pages/index", { allItems: filteredItems });
+            case "highest_price":
+                const filteredHighest = await Items.findAll({
+                    order: [["price", "ASC"]],
+                });
+                return res
+                    .status(200)
+                    .render("pages/index", { allItems: filteredHighest });
+            case "lowest_price":
+                const filteredLowest = await Items.findAll({
+                    order: [["price", "DESC"]],
+                });
+                return res
+                    .status(200)
+                    .render("pages/index", { allItems: filteredLowest });
+            default:
+                return res.send("Error");
+        }
+    },
 };
